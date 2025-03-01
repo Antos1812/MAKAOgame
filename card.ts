53ce64f53f2132ddef091d05c58d0df956276fa4
Antek <un.tek2008@gmail.com>
2025-03-01 17:40:51 +0100

    updated

--- card.ts
+++ card.ts
@@ -20,4 +20,36 @@ export enum Suit {
     King = "K",
     Ace = "A"
   }
+
+  export interface CardData {
+    suit: Suit;
+    rank: Rank;
+  }
+
+  export type CardObj = GameObj & {
+    suit: Suit;
+    rank: Rank;
+    flip: () => void;
+    highlight: (active: boolean) => void;
+  };
+
+  export function createCard(data: CardData, frame: number): CardObj {
+    const card = add ([
+        sprite("cards", {frame}),
+        pos(0,0),
+        scale(1),
+        area(),
+        {
+            suit: data.suit,
+            rank: data.rank,
+            flip() {
+                // dokonczyc logikee
+            },
+            highlight(active: boolean){
+                // tu tez dokonczyc
+            }
+        }
+    ]) as CardObj;
+    return card;
+  }
   
