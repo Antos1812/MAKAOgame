3759283a2916dd4fd49f7bc1b2ff1825f14c0133
Antek <un.tek2008@gmail.com>
2025-03-02 15:42:48 +0100

    added handString

--- main.ts
+++ main.ts
@@ -5,5 +5,6 @@ game.dealCards(5);
 
 console.log("Gives cards:");
 game.players.forEach((player) => {
+    const handString = player.hand.map(card => `${card.rank} of ${card.suit}`).join(", ");
     console.log(`${player.name}:`,player.hand);
 });
