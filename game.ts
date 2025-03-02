914dc41b8561e4a4995cbeadf85c45f5a0142190
Antek <un.tek2008@gmail.com>
2025-03-02 15:42:23 +0100

    moved shuffleDeck() to deck.ts

--- game.ts
+++ game.ts
@@ -1,6 +1,7 @@
-import { createDeck } from "./deck";
+import { createDeck, shuffleDeck } from "./deck";
 import { Player } from "./player";
 import { CardData } from "./card";
+
 
 export class Game{
     deck: CardData[];
@@ -8,16 +9,11 @@ export class Game{
 
     constructor(playerNames: string[]) {
         this.deck = createDeck();
-        this.shuffleDeck();
+        shuffleDeck(this.deck);
         this.players = playerNames.map((name) => new Player(name));
     }
 
-    shuffleDeck(): void{
-        for (let i = this.deck.length - 1; i > 0; i--){
-            const j = Math.floor(Math.random() * (i+1));
-            [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
-        }
-    }
+    
     dealCards(count: number): void{
         for (let i = 0; i<count; i++){
             this.players.forEach((player) => {
